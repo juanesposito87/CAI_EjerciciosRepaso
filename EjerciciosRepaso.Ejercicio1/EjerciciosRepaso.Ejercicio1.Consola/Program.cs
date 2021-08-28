daf2bd651bd01edaeae61a77f0a6c29c57bb3644
juanesposito87 <89708819+juanesposito87@users.noreply.github.com>
2021-08-28 20:04:11 -0300

    Ejercicio1
    Primer ejercicio de repaso

---
+++ EjerciciosRepaso.Ejercicio1/EjerciciosRepaso.Ejercicio1.Consola/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosRepaso.Ejercicio1.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string NumeroRegistroIngresado;
+            string UltimosCaracteres;
+            int UltCarConvertidos;
+            int Contador;
+            Console.Write("Ingrese el número de registro: ");
+            NumeroRegistroIngresado = Console.ReadLine();
+            Contador = 1;
+            UltimosCaracteres = NumeroRegistroIngresado.Substring(NumeroRegistroIngresado.Length - 3);
+            UltCarConvertidos = Convert.ToInt32(UltimosCaracteres);
+            string Foo = "";
+            string Bar = "";
+            for (Contador = 1; Contador < (UltCarConvertidos+1); Contador++)
+            {
+                if(Contador%3 == 0)
+                {
+                    Foo = "Foo";
+                }
+                
+                if(Contador%5 == 0)
+                {
+                    Bar = "Bar";
+                }
+                string Inter = "";
+                if((Foo.Length + Bar.Length)> 0)
+                {
+                    Inter = " - ";
+                }
+                Console.WriteLine(Convert.ToString(Contador) + Inter + Foo + Bar);
+                Foo = "";
+                Bar = "";
+                
+            }
+
+            Console.WriteLine("Presione enter para salir.");
+            Console.ReadLine();
+        }
+
+
+    }
+}
