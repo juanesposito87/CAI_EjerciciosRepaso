97cd6aaac9a0221079b2eb66bda92411b037bae3
juanesposito87 <89708819+juanesposito87@users.noreply.github.com>
2021-10-10 12:30:51 -0300

    BibliotecaUtilidades
    Se agregan validaciones y pedidos de input al usuario

--- IntegradorFacultad/IntegradorFacultad.Utilidades/Validaciones.cs
+++ IntegradorFacultad/IntegradorFacultad.Utilidades/Validaciones.cs
@@ -6,7 +6,109 @@ using System.Threading.Tasks;
 
 namespace IntegradorFacultad.Utilidades
 {
-    class Validaciones
+    public static class Validaciones
     {
+        //Validaciones de strings
+        public static string PedirString(string Campo)
+        {
+            Console.WriteLine(String.Format("Por favor, ingrese el valor para el campo {0}", Campo));
+            return Console.ReadLine();
+        }
+
+        public static bool ValidarNoVacio (string Valor)
+        {
+            return !(String.IsNullOrEmpty(Valor));
+        }
+
+        //Validaciones de Números
+        public static int PedirInt (string Campo)
+        {
+            bool Flag = false;
+            string Dato = "";
+            int IntValidado;
+            do
+            {
+                Dato = Validaciones.PedirString(Campo);
+                Flag = int.TryParse(Dato, out IntValidado);
+                if(!Flag)
+                {
+                    Console.WriteLine("Error, el dato debe ser un número entero.");
+                }
+            } while (!Flag);
+
+            return IntValidado;
+
+        }
+
+        public static byte PedirByte(string Campo)
+        {
+            bool Flag = false;
+            string Dato = "";
+            byte ByteValidado;
+            do
+            {
+                Dato = Validaciones.PedirString(Campo);
+                Flag = byte.TryParse(Dato, out ByteValidado);
+                if (!Flag)
+                {
+                    Console.WriteLine("Error, el dato debe ser un número entero entre 0 y 255.");
+                }
+            } while (!Flag);
+
+            return ByteValidado;
+
+        }
+
+        public static double PedirDouble(string Campo)
+        {
+            bool Flag = false;
+            string Dato = "";
+            double DoubleValidado;
+            do
+            {
+                Dato = Validaciones.PedirString(Campo);
+                Flag = double.TryParse(Dato, out DoubleValidado);
+                if (!Flag)
+                {
+                    Console.WriteLine("Error, el dato debe ser un número.");
+                }
+            } while (!Flag);
+
+            return DoubleValidado;
+
+        }
+
+        public static bool ValidarPositivo (int Dato)
+        {
+            return (Dato > 0);
+        }
+
+
+        public static bool ValidarPositivo(double Dato)
+        {
+            return (Dato > 0);
+        }
+
+        //Validaciones de Fechas
+
+        public static DateTime PedirFecha(string Campo)
+        {
+            bool Flag = false;
+            string Dato = "";
+            DateTime FechaValidada;
+            do
+            {
+                Dato = Validaciones.PedirString(Campo);
+                Flag = DateTime.TryParse(Dato, out FechaValidada);
+                if (!Flag)
+                {
+                    Console.WriteLine("Error, el dato debe ser una fecha válida.");
+                }
+            } while (!Flag);
+
+            return FechaValidada;
+
+        }
+
     }
 }
