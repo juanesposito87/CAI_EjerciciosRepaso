a5fc6a4deea6c1c36ac5d7cde46561af65edbee8
juanesposito87 <89708819+juanesposito87@users.noreply.github.com>
2021-11-25 17:27:38 -0300

    Plantilla
    Creación de capas, namespaces y clases recicladas.
    1. Creo la solucion winforms.
    2. Creo las tres bibliotecas de clases: Entidades, Datos y Negocio.
    3. Referencias:
    	De Interfaz, a Entidades y Negocio.
    	De Negocio, a Datos y Entidades.
    	De Datos, a Entidades.
    4. Datos -> Click derecho en Referencias -> Agregar Paquete NuGet - > NewtonSoft.JSon
    5. Agregar carpetas en Entidades: Clases, Excepciones, Utilidades.
    6. Crear clase dentro de utilidades y pegar las validaciones.
    7. Crear clase dentro de Datos y pegar el webhelper.
    
    using especiales:
    Para las validaciones: using System.Windows.Forms;
    Para clases en Entidades: system.runtime.serialization
    Para clases en Datos:
    	using System.Net.Http; using System.Collections.Specialized; using System.Net; en el webhelper;
    	collection.specialized y runtime.serialization para los mapper.

---
+++ PracticaParcial/PracticaParcial.Entidades/Utilidades/Validaciones.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaParcial.Entidades.Utilidades
+{
+    public static class Validaciones
+    {
+        //Validaciones de strings
+
+        public static bool ValidarNoVacio(string Valor)
+        {
+            return !(String.IsNullOrEmpty(Valor));
+        }
+
+
+        //Validaciones de Números
+        public static int PasarStringAInt(string Dato)
+        {
+            bool Flag = false;
+            int IntValidado;
+            do
+            {
+                Flag = int.TryParse(Dato, out IntValidado);
+                if (!Flag)
+                {
+                    throw new Exception("Error, el dato debe ser un número entero.");
+                }
+            } while (!Flag);
+
+            return IntValidado;
+
+        }
+
+        public static byte PasarStringAByte(string Dato)
+        {
+            bool Flag = false;
+            byte ByteValidado;
+            do
+            {
+                Flag = byte.TryParse(Dato, out ByteValidado);
+                if (!Flag)
+                {
+                    throw new Exception("Error, el dato debe ser un número entero entre 0 y 255.");
+                }
+            } while (!Flag);
+
+            return ByteValidado;
+
+        }
+
+        public static double PasarStringADouble(string Dato)
+        {
+            bool Flag = false;
+            double DoubleValidado;
+            do
+            {
+                Flag = double.TryParse(Dato, out DoubleValidado);
+                if (!Flag)
+                {
+                    throw new Exception("Error, el dato debe ser un número.");
+                }
+            } while (!Flag);
+
+            return DoubleValidado;
+
+        }
+
+        public static bool ValidarPositivo(int Dato)
+        {
+            return (Dato > 0);
+        }
+
+
+        public static bool ValidarPositivo(double Dato)
+        {
+            return (Dato > 0);
+        }
+
+
+        //Validaciones de Fechas
+        /*
+        public static DateTime PedirFecha(string Campo)
+        {
+            bool Flag = false;
+            string Dato = "";
+            DateTime FechaValidada;
+            do
+            {
+                Dato = Validaciones.PedirString(Campo);
+                Flag = DateTime.TryParse(Dato, out FechaValidada);
+                if (!Flag)
+                {
+                    Console.WriteLine("Error, el dato debe ser una fecha válida.");
+                }
+            } while (!Flag);
+
+            return FechaValidada;
+
+        }
+        */
+
+        public static bool FechaMayorAMinimo(DateTime Fecha, byte Minimo)
+        {
+            if ((DateTime.Now.Month >= Fecha.Month) && (DateTime.Now.Day >= Fecha.Day))
+            {
+                return ((DateTime.Now.Year - Fecha.Year) >= Minimo);
+            }
+            else
+            {
+                return ((DateTime.Now.Year - Fecha.Year - 1) >= Minimo);
+            }
+        }
+
+        public static int Antiguedad(DateTime FechaPosterior, DateTime FechaAnterior)
+        {
+            if ((FechaPosterior.Month >= FechaAnterior.Month) && (FechaPosterior.Day >= FechaAnterior.Day))
+            {
+                return (FechaPosterior.Year - FechaAnterior.Year);
+
+            }
+            else
+            {
+                return (FechaPosterior.Year - FechaAnterior.Year - 1);
+            }
+        }
+
+        public static bool FechaFutura(DateTime Fecha)
+        {
+            return (Fecha > DateTime.Now);
+        }
+
+        //Validaciones de Booleanos
+
+        /*
+        public static bool PedirBool(string Campo)
+        {
+            string input = "";
+            bool Devolver = false;
+            do
+            {
+                Console.WriteLine(String.Format("Por favor, ingrese el valor para el campo {0} (S/N)", Campo));
+                input = Console.ReadLine();
+                if ((input == "S") || (input == "s"))
+                {
+                    Devolver = true;
+                }
+                else if ((input == "N") || (input == "n"))
+                {
+                    Devolver = false;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada incorrecta, por favor reintente.");
+                }
+            } while (input != "N" || input != "n" || input != "S" || input != "s");
+
+            return Devolver;
+        }
+
+        */
+
+        public static string BoolASiNo(bool Dato)
+        {
+            if (Dato)
+            {
+                return "SI";
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+
+        public static void Validar(ComboBox cb)
+        {
+            if (cb.SelectedIndex == 0)
+            {
+                throw new Exception("Seleccione un valor de la lista desplegable.");
+            }
+                
+        }
+        public static void Validar(ListBox lb)
+        {
+            if(lb.SelectedIndex == -1)
+            {
+                throw new Exception("Debe seleccionar un valor de la lista.");
+            }   
+
+        }
+
+        public static void Validar(TextBox tb)
+        {
+            if (string.IsNullOrEmpty(tb.Text))
+                throw new Exception("Debe completar el campo de texto.");
+        }
+
+    }
+}
